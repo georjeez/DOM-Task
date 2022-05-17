d974a10add351dc2e35b84db3d4786bc589e8af1
Georgie <92385575+georjeez@users.noreply.github.com>
2022-05-17 19:52:14 +0100

    completed dom task

--- main.js
+++ main.js
@@ -4,9 +4,22 @@ function roundUp(x) {
 }
 
 function CalculateMagic(a, b) {
-    // add your code here
+    let number = a + b + b;
     
+    number = number + 90; // add 90
     
-    // remember to call roundUp() with your result before returning
-    // return roundUp(g);
+    number = number / 10; // divide by 10
+    
+    if(number % 5 === 0){ // check if number is divisible by 5
+        number = number - 8; // if multiple of 5, subtract 8
+    }
+    else {
+        number = number + 9; // if not mmultiple of 5, add 9
+    }
+
+    number = number * number; // square the answer
+
+    let G = number / 3; // divide by 3, create variable named "G"
+
+    return roundUp(G);
 }
